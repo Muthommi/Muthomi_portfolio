d8761ed8ea9eee8987541a8241bb684ebb6a151f
Muthommi <kelvinmartin16@gmail.com>
2025-08-08 12:59:38 +0300

    Improving my portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js, ts, jsx, tsx}"],
+  content: ['./index.html', './src/**/*.{js, ts, jsx, tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        'bg-light': 'var(--color-bg-light)',
+        'bg-dark': 'var(--color-bg-dark)',
+        'text-light': 'var(--color-text-light)',
+        'text-dark': 'var(--color-text-dark)'
+      },
+      borderRadius: {
+        DEFAULT: 'var(--radius-default)'
+      },
+      fontFamily: {
+	      sans: ['Inter', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
